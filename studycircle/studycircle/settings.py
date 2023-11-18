93d8bd24d6be2ff3c6f60db53386221fce4803e7
Ervin Lanada <ervinjon_lanada@outlook.com>
2023-11-18 12:07:59 -0700

    Add apps in settings and urls.py

--- studycircle/studycircle/settings.py
+++ studycircle/studycircle/settings.py
@@ -31,6 +31,11 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users',
+    'locations',
+    'groups',
+    'courses',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- studycircle/studycircle/urls.py
+++ studycircle/studycircle/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", include("users.urls")),
+    path("groups/", include("groups.urls")),
+    path("locations/", include("locations.urls")),
+    path("courses/", include("courses.urls"))
 ]
