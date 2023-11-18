89ea900faad7a492fa191cdd4789c43a55d194b0
Ervin Lanada <ervinjon_lanada@outlook.com>
2023-11-18 11:53:24 -0700

    Models for the user app

--- studycircle/users/models.py
+++ studycircle/users/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+    major = models.CharField(max_length=100)
+    degree = models.CharField(max_length=100)
