d73b0f24d6e33f5c2912978dc8f88cf17e04165f
EJ-Lan <ervinjon_lanada@outlook.com>
2023-11-18 21:46:57 -0700

    make profile page

--- studycircle/users/urls.py
+++ studycircle/users/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
     path("profile", views.profile_view, name="profile"),
-    path("edit_profile", views.edit_profile_view, name="edit_profile")
+    path("edit_profile", views.edit_profile_view, name="edit_profile"),
+    path('add_course/', views.add_course_view, name='add_course'),
 ]
--- studycircle/users/views.py
+++ studycircle/users/views.py
@@ -5,7 +5,10 @@ from django.contrib.auth import authenticate, login, logout
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 
-from .models import User
+from groups.models import UserGroup
+from courses.models import Course
+
+from .models import User, UserCourse
 
 # Create your views here.
 def login_view(request):
@@ -67,13 +70,19 @@ def register(request):
         return render(request, "users/register.html")
     
 def profile_view(request):
+    user = request.user
+    enrolled_courses = UserCourse.objects.filter(user=user)
+    enrolled_groups = UserGroup.objects.filter(user=user)
     return render(request, "users/profile.html", {
-        "user": request.user
+        "user": user,
+        "enrolled_courses": enrolled_courses,
+        "enrolled_groups": enrolled_groups
     })
 
 def edit_profile_view(request):
     if request.method == "POST":
         new_username = request.POST["new_username"]
+        new_email = request.POST["new_email"]
         major = request.POST["major"]
         degree = request.POST["degree"]
         user = request.user
@@ -82,30 +91,41 @@ def edit_profile_view(request):
         user.major = major
         user.degree = degree
 
-        # Check if the new username is different from the current one
-        if new_username != request.user.username:
-            # Check if the new username is available
-            if User.objects.filter(username=new_username).exists():
-                return render(request, "users/edit_profile.html", {
-                    "user": request.user,
-                    "message": "Username is already taken."
+        # Check if the new username or email is different from the current one
+        if new_username != user.username or new_email != user.email:
+            # Check if the new username or email is available
+            if User.objects.filter(username=new_username).exists() or User.objects.filter(email=new_email).exists():
+                return render(request, "users/profile.html", {
+                    "user": user,
+                    "message": "Username or email is already taken."
                 })
 
-            # Update Username
+            # Update Username and Email
             user.username = new_username
+            user.email = new_email
             user.save()
 
             return render(request, "users/profile.html", {
-                "user": request.user,
-                "message": "Username, Major and Degree successfully changed."
+                "user": user,
+                "message": "Profile updated successfully."
             })
-        
+
         else:
-            return render(request, "users/edit_profile.html", {
-                "user": request.user,
-                "message": "Username needs to be different from previous Username."
+            return render(request, "users/profile.html", {
+                "user": user,
+                "message": "No changes detected in Username or Email."
             })
     else:
-        return render(request, "users/edit_profile.html", {
+        return render(request, "users/profile.html", {
             "user": request.user
         })
+    
+def add_course_view(request):
+    if request.method == "POST":
+        course_id = request.POST["course_id"]
+        course = Course.objects.get(id=course_id)
+        user_course = UserCourse(user=request.user, course=course)
+        user_course.save()
+        return HttpResponseRedirect(reverse("users:profile"))
+
+    return render(request, "users/profile.html")
